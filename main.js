433db80935f57996f079a218c1477c31ab1559e7
Isaacjcollier <isaacjcollier@gmail.com>
2016-06-14 14:00:00 +0100

    some examples for callbacks

---
+++ main.js
@@ -0,0 +1,7 @@
+var helpers = require('./utilities.js')
+
+helpers.max([1, 2, 3], function (result) {
+  console.log(result)
+})
+
+helpers.min()
---
+++ utilities.js
@@ -0,0 +1,19 @@
+var num = [5, 7, 8, 10]
+
+function max (arr, callback) {
+  var maxValue = arr[0]
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxValue) {
+      maxValue = arr[i]
+    }
+  } return callback(maxValue)
+}
+
+function min () {
+  return 'not doing anything'
+}
+
+module.exports = {
+  max: max,
+  min: min
+}
